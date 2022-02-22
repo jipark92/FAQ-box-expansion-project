455c0dc4c2554d1db9cfa5a95b9beb671c75e22e
ji <jiyongprk@gmail.com>
2022-02-22 18:03:51 -0500

    this is hard

---
+++ main.js
@@ -0,0 +1,22 @@
+const answerOneBtn = document.querySelector('.answer-1');
+const answerTWoBtn = document.querySelector('.answer-2');
+const answerThreeBtn = document.querySelector('.answer-3');
+const answerFourBtn= document.querySelector('.answer-4');
+const answerFiveBtn = document.querySelector('.answer-5');
+
+const questionOneBox = document.querySelector('.question-1');
+const questionTwoBox = document.querySelector('.question-2');
+const questionThreeBox = document.querySelector('.question-3');
+const questionFourBox = document.querySelector('.question-4');
+const questionFiveBox = document.querySelector('.question-5');
+
+
+
+
+// answerOneBtn.addEventListener('click', ()=> {
+//     let questionAns = document.createElement('div');
+//     questionAns.setAttribute('class', 'answer-show-1');
+//     questionOneBox.appendChild(questionAns);
+//     questionAns.textContent ="This is the answer";
+//     console.log(questionAns);
+// })
