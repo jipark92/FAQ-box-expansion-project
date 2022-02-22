7e42e8cf5e9a33e5e24409ecbd6cf8e167aa8f12
ji <jiyongprk@gmail.com>
2022-02-22 18:37:38 -0500

    open/close question answer working

--- main.js
+++ main.js
@@ -1,22 +1,15 @@
-const answerOneBtn = document.querySelector('.answer-1');
-const answerTWoBtn = document.querySelector('.answer-2');
-const answerThreeBtn = document.querySelector('.answer-3');
-const answerFourBtn= document.querySelector('.answer-4');
-const answerFiveBtn = document.querySelector('.answer-5');
+const answerOneBtn = document.querySelector('.answer-1-btn');
+const answerTwoBtn = document.querySelector('.answer-2-btn');
+const answerThreeBtn = document.querySelector('.answer-3-btn');
 
-const questionOneBox = document.querySelector('.question-1');
-const questionTwoBox = document.querySelector('.question-2');
-const questionThreeBox = document.querySelector('.question-3');
-const questionFourBox = document.querySelector('.question-4');
-const questionFiveBox = document.querySelector('.question-5');
+const showAnswerOne = document.querySelector('.answer-1');
+const showAnswerTwo = document.querySelector('.answer-2');
+const showAnswerThree = document.querySelector('.answer-3');
 
 
+answerOneBtn.addEventListener('click',()=>{
+    showAnswerOne.classList.toggle('answer-1');
+    console.log(showAnswerOne);
+})
 
 
-// answerOneBtn.addEventListener('click', ()=> {
-//     let questionAns = document.createElement('div');
-//     questionAns.setAttribute('class', 'answer-show-1');
-//     questionOneBox.appendChild(questionAns);
-//     questionAns.textContent ="This is the answer";
-//     console.log(questionAns);
-// })
